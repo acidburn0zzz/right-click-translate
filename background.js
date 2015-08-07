ef3ad47f6443a746602c48e78833d148cccbf167
Eric Fan <eric.fan@alcidion.com>
2015-08-07 16:25:42 +0930

    Open new tab next to the current one.

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
 chrome.contextMenus.create({
-	title : "Translate '%s'",
-	contexts : ["selection"],
-	onclick : function translate(info) {
-		chrome.tabs.create({
-			url : 'http://translate.google.com.au/#auto/#/' + encodeURIComponent(info.selectionText)
+	title: "Translate '%s'",
+	contexts: ["selection"],
+	onclick: function translate(info) {
+		chrome.tabs.query({ active: true }, function (tabs) {
+			chrome.tabs.create({
+				index: tabs[0]['index'] + 1,
+				url: 'http://translate.google.com.au/#auto/#/' + encodeURIComponent(info.selectionText)
+			});
 		});
 	}
 });
