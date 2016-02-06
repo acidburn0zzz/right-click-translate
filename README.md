4aa128015310fd525e28ec0f2f949761277be569
eric <ericvan76@gmail.com>
2016-02-06 20:59:46 +1030

    Added google analytics

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-##Right-Click Translate
+##Right Click Translate
 
-Right-Click to translate selected text into a new Tab using Google Translate
+Right Click to translate selected text into a new Tab using Google Translate
 
 Download: [http://goo.gl/Rcjs82](http://goo.gl/Rcjs82)
 
--- background.js
+++ background.js
@@ -1,12 +1,42 @@
+// Google Analytics
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+// Create GA tracker
+ga('create', 'UA-73153112-4', 'auto');
+
+// Add context menu
 chrome.contextMenus.create({
-	title: "Translate '%s'",
-	contexts: ["selection"],
-	onclick: function translate(info) {
-		chrome.tabs.query({ active: true }, function (tabs) {
-			chrome.tabs.create({
-				index: tabs[0].index + 1,
-				url: 'http://translate.google.com.au/#auto/#/' + encodeURIComponent(info.selectionText)
-			});
-		});
-	}
+  title: "Translate '%s'",
+  contexts: ["selection"],
+  onclick: function translate(info) {
+
+    // Create new Tab
+    chrome.tabs.query({
+      active: true
+    }, function(tabs) {
+      chrome.tabs.create({
+        index: tabs[0].index + 1,
+        url: 'http://translate.google.com.au/#auto/#/' + encodeURIComponent(info.selectionText)
+      });
+    });
+
+    // Send event
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'translate',
+      eventAction: 'click',
+      nonInteraction: true
+    });
+
+  }
 });
