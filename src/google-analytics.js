6800a09eb47b4d7bbcb66b2992ee7f8ff668fc63
eric <ericvan76@gmail.com>
2018-05-31 22:18:16 +0930

    fix lint

--- src/google-analytics.js
+++ src/google-analytics.js
@@ -1,3 +1,5 @@
+// tslint:disable
+
 (function (i, s, o, g, r, a, m) {
   i['GoogleAnalyticsObject'] = r;
   i[r] = i[r] || function () {
@@ -12,3 +14,5 @@
 
 // Create GA tracker
 ga('create', 'UA-73153112-5', 'auto');
+
+// tslint:enable
